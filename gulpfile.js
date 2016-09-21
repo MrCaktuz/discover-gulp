3cd9cb937893b07f75aab49a6171103a5ee64d42
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-09-21 11:22:15 +0200

    imagemin&com
    Added the dependance imagemin and added comments in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
+// Définition des dépendance dont on a besoin pour exécuter les taches.
 var
-    gulp = require('gulp');
+    gulp = require( 'gulp' ),
+    imagemin = require( 'gulp-imagemin' );
 
+// Définition de quelques variables générales pour notre gulpfile.
+var
+    source = 'source/',
+    dest = 'build/'
+
+// Définition de quelques variables liées à nos taches (options de taches).
+var
+    imagesOpts = {
+
+    };
+
+// Définition des taches.
+gulp.task( 'images', function(){
+    gulp.src()
+} )
+
+// Tache par défault exécuté lorsqu'on tape juste gulp dans le terminal.
 gulp.task('default', function(){
     console.log('test');
 });
